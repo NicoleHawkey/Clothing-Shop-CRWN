e1a26a52d2fa518885838aa057ff82f57a16ea6e
Nicole Hawkey <n.laggner@gmx.at>
2023-09-05 16:46:54 +1200

    creating sign-in-form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -85,7 +85,7 @@ const SignUpForm = () => {
                     name='confirmPassword' 
                     value={confirmPassword}
                 />
-                <Button type="submit">Sign up</Button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )
